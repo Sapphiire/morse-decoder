ed1227f11b7c72100c927b0bd2562f6b8d59bdc6
Vadzim <Sapphiire@github.com>
2020-10-13 12:59:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,14 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    
+    let words = expr.split(/\s*[\*]+\s*/).map(el1 => el1.match(/.{10}/g).map(el2 => el2.match(/.{2}/g)))
+    return words.map(el1 => el1.map(el2 => el2.reduce(
+        (accumulator, current) => {
+            if(current === '10') return accumulator + ".";
+            if(current === '11') return accumulator + "-";
+            return accumulator;
+    }, "")).map(el2 => MORSE_TABLE[el2]).join('')
+    ).join(' ')
 }
 
 module.exports = {
